5b007fceb1f5726931185cc0fc78a482cc9746db
reijjo <teemu.aitomeri@gmail.com>
2024-11-19 13:23:54 +0200

    reporter stuff

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -27,11 +27,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [["github"], ["html", { outputFolder: "playwright-report" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: BASE_URL ?? "http://localhost:3000",
+
+    /* Video for failed tests */
+    video: "retain-on-failure",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
