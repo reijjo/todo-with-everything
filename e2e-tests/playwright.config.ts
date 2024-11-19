01d1852e1804139c9483205d1da30ea2fe2f41fc
reijjo <teemu.aitomeri@gmail.com>
2024-11-19 12:37:46 +0200

    ???

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -11,8 +11,6 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 const BASE_URL =
   process.env.NODE_ENV === "test" ? process.env.TEST_URL : process.env.BASE_URL;
 
-console.log("BASE_URL", BASE_URL);
-
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -33,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
