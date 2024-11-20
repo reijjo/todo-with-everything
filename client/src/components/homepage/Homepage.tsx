1747f8781e2a548d9b381c118f34243e31d67dec
reijjo <teemu.aitomeri@gmail.com>
2024-11-20 14:58:15 +0200

    textinputs done

--- client/src/components/homepage/Homepage.tsx
+++ client/src/components/homepage/Homepage.tsx
@@ -1,5 +1,6 @@
 import "./Homepage.css";
 
+import { TextInput } from "../common";
 import { Todo } from "./Todo";
 
 export const Homepage = () => {
@@ -8,6 +9,17 @@ export const Homepage = () => {
       <div className="align-center">
         <h1>To-do list</h1>
         <Todo />
+        <div className="single-todo">
+          <TextInput
+            label="create todo"
+            name="todo"
+            id="todo"
+            placeholder="What to do..."
+            value="heih"
+            // onChange={handleTodoChange}
+            width="50%"
+          />
+        </div>
       </div>
     </main>
   );
---
+++ client/src/utils/types.ts
@@ -0,0 +1,11 @@
+export interface StyleProps {
+  width?: string;
+  height?: string;
+  border?: string;
+  borderColor?: string;
+  color?: string;
+  backgroundColor?: string;
+  boxShadow?: string;
+  display?: string;
+  justifyContent?: string;
+}
