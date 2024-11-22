4406b317ed8effaee97a5a315c09940069087f91
reijjo <teemu.aitomeri@gmail.com>
2024-11-22 14:58:49 +0200

    viteconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "./",
+  base: "./",
   test: {
     globals: true,
     environment: "jsdom",
