791b063f4ada611542ab20f57438ea7c79620fb6
reijjo <teemu.aitomeri@gmail.com>
2024-11-24 09:26:26 +0200

    all ok?

--- server/src/utils/db/db.ts
+++ server/src/utils/db/db.ts
@@ -3,8 +3,9 @@ import { config } from "../config";
 import { cyanBright, yellowBright } from "colorette";
 
 const { DATABASE_URL, DATABASE_TEST_URL } = config;
-const isTestEnv = Bun.env.NODE_ENV === "test" || Bun.env.NODE_ENV === "testcicd"
-const URL =  isTestEnv ? DATABASE_TEST_URL : DATABASE_URL;
+// const isTestEnv = Bun.env.NODE_ENV === "test" || Bun.env.NODE_ENV === "testcicd"
+// const URL =  isTestEnv ? DATABASE_TEST_URL : DATABASE_URL;
+const URL = Bun.env.NODE_ENV === "test" ? DATABASE_TEST_URL : DATABASE_URL;
 
 console.log("DATA", URL);
 console.log(yellowBright("Environment:"), Bun.env.NODE_ENV);
@@ -27,11 +28,11 @@ export const connectToDB = async () => {
     await sequelize.authenticate();
     console.log(cyanBright("Connected to database."));
 
-		if (isTestEnv) {
-      console.log(yellowBright("Test environment detected - syncing database..."));
-      await sequelize.sync({ force: true }); // This will drop and recreate tables
-      console.log(cyanBright("Test database synced."));
-    }
+		// if (isTestEnv) {
+    //   console.log(yellowBright("Test environment detected - syncing database..."));
+    //   await sequelize.sync({ force: true }); // This will drop and recreate tables
+    //   console.log(cyanBright("Test database synced."));
+    // }
   } catch (error: unknown) {
     console.error("Unable to connect to the database:", error);
     return process.exit(1);
